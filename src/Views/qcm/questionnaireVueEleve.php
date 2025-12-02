5acc6c5de70b2a3626f721084d154503f5ea79a9
Matéo deterne <mateodeterne@gmail.com>
2025-12-02 14:58:14 +0000

    ajout TODO pour la vue questionnaire eleve

--- src/Views/qcm/questionnaireVueEleve.php
+++ src/Views/qcm/questionnaireVueEleve.php
@@ -1,4 +1,7 @@
 <?php
 //TODO affiche le questionnaire pour l'élève
+// piste pour celui-ci : bas de la page + boucle while avec a chaque fois un teste sur le type de question
+// comme ça on a la base qui change pas et le contenu qui dépendra des questions (vous pouvez récupérer celles-ci grace a la variable $questionQuestionnaire initialiser dans le home controlleur)
+// la seul fonction de cette page est d'afficher le questionnaire pour l'élève donc les seuls acces son -pin et -QRcode(qui lancera la page avec le pin correspondant).
 
 ?>

b727cdbacb2054f5d9515a933f3d8bff8c0fe471
LOI-mln <milan.loi@etu.unilim.fr>
2025-12-08 09:10:48 +0100

    feat(dashboard): Implement Dashboard controller, view, and Vue.js logic

--- src/Controleurs/tableauDeBordControlleur.php
+++ src/Controleurs/tableauDeBordControlleur.php
@@ -1,41 +1,48 @@
 <?php
 
-class tableauDeBordControlleur {
+class tableauDeBordControlleur
+{
 
-    function index() {
-        require_once(__DIR__.'/../Views/espace_perso/dashboard.php');
+    function index()
+    {
+        require_once(__DIR__ . '/../Views/espace_perso/dashboard.php');
     }
 
-    function getMesQuestionnaires() {
+    function getMesQuestionnaires()
+    {
         // //TODO (Back-End): Renvoyer les vraies données
         header('Content-Type: application/json');
         echo json_encode([]); // Renvoie un tableau vide
         exit;
     }
 
-    function creerNouveau() {
+    function creerNouveau()
+    {
 
         // TODO (Back-End): Créer en BDD et renvoyer le vrai ID
         header('Content-Type: application/json');
         echo json_encode([
             'status' => 'success_mock',
-            'nouveau_id' => 999 
+            'nouveau_id' => 999
         ]);
         exit;
     }
 
-    function supprimer() {
+    function supprimer()
+    {
         // TODO (Back-End): Supprimer en BDD
         header('Content-Type: application/json');
         echo json_encode(['status' => 'success_mock']);
         exit;
     }
 
-    function conditionGenerales() {
-        require_once __DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'Views'.DIRECTORY_SEPARATOR.'legal'.DIRECTORY_SEPARATOR.'conditionsGenerales.php';
+    function conditionGenerales()
+    {
+        require_once __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'Views' . DIRECTORY_SEPARATOR . 'legal' . DIRECTORY_SEPARATOR . 'conditionGenerales.php';
     }
 
-    function confidentialite() {
-        require_once __DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'Views'.DIRECTORY_SEPARATOR.'legal'.DIRECTORY_SEPARATOR.'confidentialite.php';
+    function confidentialite()
+    {
+        require_once __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'Views' . DIRECTORY_SEPARATOR . 'legal' . DIRECTORY_SEPARATOR . 'confidentialite.php';
     }
 }

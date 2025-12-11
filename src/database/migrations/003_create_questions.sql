3ef330d94a5a8ac33cced241f2eb0a588f3ada05
MESRI <ahmed.mesri@etu.unilim.fr>
2025-12-11 15:03:32 +0100

    Modification table questionnaire

--- src/database/migrations/003_create_questions.sql
+++ src/database/migrations/003_create_questions.sql
@@ -1,7 +1,7 @@
 CREATE TABLE questions (
     id INT AUTO_INCREMENT PRIMARY KEY,
     survey_id int NOT NULL,
-    type ENUM('text', 'single_choice', 'multiple_choice') NOT NULL,
+    type ENUM('short_text', 'long_text', 'scale', 'single_choice', 'multiple_choice') NOT NULL,
     label TEXT NOT NULL,
     order_index INT DEFAULT 0,
     is_required BOOLEAN DEFAULT FALSE,

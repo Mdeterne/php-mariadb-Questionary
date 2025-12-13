e003f58500b689486a87fd33639540cbe28c6c7b
LOI-mln <milan.loi@etu.unilim.fr>
2025-12-13 22:09:48 +0100

    Suppression des fichiers de débogage

--- debug_schema.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once __DIR__ . '/src/Models/Database.php';
-$pdo = (new Database())->getConnection();
-$stmt = $pdo->query("DESCRIBE responses");
-print_r($stmt->fetchAll(PDO::FETCH_ASSOC));

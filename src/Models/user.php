518501bee0f033751495c27faaedf9e1938427d8
LOI-mln <milan.loi@etu.unilim.fr>
2025-12-15 15:50:59 +0100

    ajout migration auto_increment

--- src/Models/user.php
+++ src/Models/user.php
@@ -2,18 +2,21 @@
 
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'Database.php';
 
-class User {
+class User
+{
     private $conn;
 
-    public function __construct() {
+    public function __construct()
+    {
         $database = new Database();
         $this->conn = $database->getConnection();
         if ($this->conn === null) {
             throw new Exception('Erreur de connexion à la base de données.');
         }
     }
-    
-    function findbyEmail($email) {
+
+    function findbyEmail($email)
+    {
         $query = "SELECT * FROM users WHERE email = :email LIMIT 1";
         $stmt = $this->conn->prepare($query);
         $stmt->bindParam(':email', $email);
@@ -21,7 +24,8 @@ class User {
         return $stmt->fetch(PDO::FETCH_ASSOC);
     }
 
-    function findById($id) {
+    function findById($id)
+    {
         $query = "SELECT * FROM users WHERE id = :id LIMIT 1";
         $stmt = $this->conn->prepare($query);
         $stmt->bindParam(':id', $id);
@@ -29,7 +33,8 @@ class User {
         return $stmt->fetch(PDO::FETCH_ASSOC);
     }
 
-    function createUserIfNotExists($id,$email, $name) {
+    function createUserIfNotExists($id, $email, $name)
+    {
         $existingUser = $this->findbyEmail($email);
         if ($existingUser) {
             return $existingUser['id'];
@@ -40,7 +45,7 @@ class User {
             $stmt->bindParam(':email', $email);
             $stmt->bindParam(':full_name', $name);
             $stmt->execute();
-            return $this->conn->lastInsertId();
+            return $id;
         }
     }
 }

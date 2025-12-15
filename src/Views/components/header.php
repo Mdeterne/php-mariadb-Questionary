faefa26b71c52f350e41321f8ab8142d9d9c2565
Jules GAYET <jules.gayet12@gmail.com>
2025-12-15 16:30:29 +0100

    retrait du bouton créer un questionnaire

--- src/Views/components/header.php
+++ src/Views/components/header.php
@@ -4,13 +4,10 @@
             <span class="appicon" aria-hidden="true"></span>
             <span class="apptitle">QUESTIONARY</span>
         </a>
-        <?php if (isset($showCreateLink) && $showCreateLink): ?>
-            <a class="topbar__create-link" href="?c=tableauDeBord">Créer un questionnaire</a>
-        <?php endif; ?>
     </div>
 
     <div class="topbar__right" aria-label="Université de Limoges">
         <span class="uni-badge" aria-hidden="true">uℓ</span>
         <span class="uni-text">Université de Limoges</span>
     </div>
-</header>
+</header>   

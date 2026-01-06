a5b8088c6dacac71aae8157e40b5f5b3758c1e11
LOI-mln <milan.loi@etu.unilim.fr>
2026-01-06 10:03:00 +0100

    Set default session role and format code

--- index.php
+++ index.php
@@ -6,108 +6,112 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require_once __dir__.DIRECTORY_SEPARATOR."src".DIRECTORY_SEPARATOR."Configues".DIRECTORY_SEPARATOR."configue_CAS.php";
+require_once __DIR__ . DIRECTORY_SEPARATOR . "src" . DIRECTORY_SEPARATOR . "Configues" . DIRECTORY_SEPARATOR . "configue_CAS.php";
 
 $_SESSION['mail'] = $infoSESSION['mail'];
 $_SESSION['name'] = $infoSESSION['cn'];
 $_SESSION['id'] = $infoSESSION['uid'];
 
+if (!isset($_SESSION['role'])) {
+  $_SESSION['role'] = 'etudiant';
+}
+
 
 // Controllers
-require_once __dir__.DIRECTORY_SEPARATOR."src".DIRECTORY_SEPARATOR."Controleurs".DIRECTORY_SEPARATOR."homeControleur.php";
-require_once __dir__.DIRECTORY_SEPARATOR."src".DIRECTORY_SEPARATOR."Controleurs".DIRECTORY_SEPARATOR."tableauDeBordControlleur.php";
-require_once __dir__.DIRECTORY_SEPARATOR."src".DIRECTORY_SEPARATOR."Controleurs".DIRECTORY_SEPARATOR."creationQuestionnaireControleur.php";
-require_once __dir__.DIRECTORY_SEPARATOR."src".DIRECTORY_SEPARATOR."Controleurs".DIRECTORY_SEPARATOR."espaceAnalyseControleur.php";
+require_once __DIR__ . DIRECTORY_SEPARATOR . "src" . DIRECTORY_SEPARATOR . "Controleurs" . DIRECTORY_SEPARATOR . "homeControleur.php";
+require_once __DIR__ . DIRECTORY_SEPARATOR . "src" . DIRECTORY_SEPARATOR . "Controleurs" . DIRECTORY_SEPARATOR . "tableauDeBordControlleur.php";
+require_once __DIR__ . DIRECTORY_SEPARATOR . "src" . DIRECTORY_SEPARATOR . "Controleurs" . DIRECTORY_SEPARATOR . "creationQuestionnaireControleur.php";
+require_once __DIR__ . DIRECTORY_SEPARATOR . "src" . DIRECTORY_SEPARATOR . "Controleurs" . DIRECTORY_SEPARATOR . "espaceAnalyseControleur.php";
 
-$controleur = isset($_GET['c'])? $_GET['c'] : 'home';
-$action = isset($_GET['a'])? $_GET['a'] : 'index';
-$questionaire = isset($_GET['q'])? $_GET['q'] : '0';
-$pin = isset($_GET['pin'])? $_GET['pin'] : '';
+$controleur = isset($_GET['c']) ? $_GET['c'] : 'home';
+$action = isset($_GET['a']) ? $_GET['a'] : 'index';
+$questionaire = isset($_GET['q']) ? $_GET['q'] : '0';
+$pin = isset($_GET['pin']) ? $_GET['pin'] : '';
 
 
-switch ($controleur){
-  
+switch ($controleur) {
+
   case 'home':
     $homeControleur = new homeControleur();
-    switch ($action){
+    switch ($action) {
       case 'index':
         $homeControleur->index();
-      break;
+        break;
 
       case 'valider':
         $homeControleur->valider($pin);
-      break;
+        break;
 
       case 'saveReponse':
         $homeControleur->saveReponse();
-      break;
+        break;
 
       case 'merci':
         $homeControleur->merci();
-      break;
+        break;
 
       case 'conditionGenerales':
         $homeControleur->conditionGenerales();
-      break;
+        break;
 
       case 'confidentialite':
         $homeControleur->confidentialite();
-      break;
+        break;
 
       case 'utilisationCookie':
         $homeControleur->utilisationCookie();
-      break;
+        break;
     }
-  break;
+    break;
 
   case 'tableauDeBord':
-  $tableauDeBordControlleur = new tableauDeBordControlleur();
-  $creationQuestionnaireControleur = new creationQuestionnaireControleur();
-  if($_SESSION['role'] != 'enseignant'){
-    $controleur = 'home';
-    $action = null;
-  }
-  switch ($action){
-          
-    case 'index':
-      // var_dump($_SESSION);
-      $tableauDeBordControlleur->index();
+    $tableauDeBordControlleur = new tableauDeBordControlleur();
+    $creationQuestionnaireControleur = new creationQuestionnaireControleur();
+    if ($_SESSION['role'] != 'enseignant') {
+      $controleur = 'home';
+      $action = null;
+    }
+    switch ($action) {
+
+      case 'index':
+        // var_dump($_SESSION);
+        $tableauDeBordControlleur->index();
+        break;
+
+      case 'getMesQuestionnaires':
+        $tableauDeBordControlleur->getMesQuestionnaires();
+        break;
+
+      case 'creerNouveau':
+        $tableauDeBordControlleur->creerNouveau();
+        break;
+
+      case 'supprimer':
+        $tableauDeBordControlleur->supprimer();
+        break;
+
+      case 'importer':
+        $creationQuestionnaireControleur->import($pin);
+        break;
+
+      case 'parametres':
+        $tableauDeBordControlleur->parametres();
+        break;
+
+      case 'saveSettings':
+        $tableauDeBordControlleur->saveSettings();
+        break;
+
+
+
+
+
+    }
     break;
-          
-    case 'getMesQuestionnaires':
-      $tableauDeBordControlleur->getMesQuestionnaires();
-    break;
-
-    case 'creerNouveau':
-      $tableauDeBordControlleur->creerNouveau();
-    break;
-        
-    case 'supprimer':
-      $tableauDeBordControlleur->supprimer();
-    break;
-
-    case 'importer':
-      $creationQuestionnaireControleur->import($pin);
-    break;
-
-    case 'parametres':
-      $tableauDeBordControlleur->parametres();
-    break;
-
-    case 'saveSettings':
-      $tableauDeBordControlleur->saveSettings();
-    break;
-
-
-
-
-
-  }
-  break;
 
   case 'espaceAnalyse':
     $espaceAnalyseControleur = new espaceAnalyseControleur();
-    if($_SESSION['role'] != 'enseignant'){
+    if ($_SESSION['role'] != 'enseignant') {
       $controleur = 'home';
       $action = null;
       exit();
@@ -116,42 +120,42 @@ switch ($controleur){
       case 'index':
       default:
         $espaceAnalyseControleur->index();
-      break;
+        break;
     }
-  break;
+    break;
 
   case 'createur':
     $creationQuestionnaireControleur = new creationQuestionnaireControleur();
-    if($_SESSION['role'] != 'enseignant'){
+    if ($_SESSION['role'] != 'enseignant') {
       $controleur = 'home';
       $action = null;
     }
 
-    switch ($action){
+    switch ($action) {
       case 'nouveauFormulaire':
         $creationQuestionnaireControleur->nouveauFormulaire();
-      break;
+        break;
 
       case 'index':
         $creationQuestionnaireControleur->index();
-      break;
+        break;
 
       case 'save':
         $creationQuestionnaireControleur->save();
-      break;
+        break;
 
       case 'editer':
         $creationQuestionnaireControleur->editer();
-      break;
+        break;
     }
-  break;
+    break;
 
   case 'parametre':
-    if($_SESSION['role'] != 'enseignant'){
+    if ($_SESSION['role'] != 'enseignant') {
       $controleur = 'home';
       $action = null;
     }
     $tableauDeBordControlleur = new tableauDeBordControlleur();
     $tableauDeBordControlleur->parametres();
-  break;
+    break;
 }

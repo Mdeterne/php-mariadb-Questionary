ddc0fb73d54989e2111057aafcf5748ada11823f
Mateo Deterne <mateodeterne@gmail.com>
2026-01-12 14:59:38 +0100

    désactivation de l'affichage des erreurs

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 
 session_start();
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+ini_set('display_errors', 0);
+ini_set('display_startup_errors', 0);
+error_reporting(0);
 
 require_once __DIR__ . DIRECTORY_SEPARATOR . "src" . DIRECTORY_SEPARATOR . "Configues" . DIRECTORY_SEPARATOR . "configue_CAS.php";
 

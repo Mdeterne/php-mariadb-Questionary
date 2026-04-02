b26c5f432a34679456e47a6e26d7bd1ef418f640
João Santos <joao.santos@etu.unilim.fr>
2026-04-02 10:58:47 +0000

    intégration du support des variables CSS dans la vue étudiant

--- src/Views/qcm/questionnaireVueEleve.php
+++ src/Views/qcm/questionnaireVueEleve.php
@@ -8,6 +8,8 @@
     <link rel="stylesheet" href="style.css">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="stylesheet" href="css/variables.css">
+    <link rel="stylesheet" href="css/components/topbar.css">
     <link rel="stylesheet" href="css/pages/questionnaire_eleve.css">
 </head>
 
